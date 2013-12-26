5138d331fc56d99a2143fad3640d01bee3115b90
James Wu <wuj818@gmail.com>
2013-12-25 21:47:56 -0500

    fixing markup and css for icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
-  def nav_class(controller)
-    controller.to_s == controller_name ? 'active' : nil
-  end
-
   def currency(number)
     amount = "$#{number_with_delimiter number.abs}"
     number < 0 ? "-#{amount}" : amount
+  end
+
+  def icon(name)
+    content_tag :span, nil, class: "glyphicon glyphicon-#{name.to_s.dasherize}"
+  end
+
+  def nav_class(controller)
+    controller.to_s == controller_name ? 'active' : nil
   end
 end
