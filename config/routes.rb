b560c05f7929f7cf15a60972332ad7bd2bc2cf25
James Wu <wuj818@gmail.com>
2014-11-12 22:42:01 -0800

    running rake rails:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Pecuniary::Application.routes.draw do
+Rails.application.routes.draw do
   resources :asset_snapshots, path: 'asset-snapshots', only: [:show, :edit, :update, :destroy]
 
   resources :contributions, only: [:index, :show, :edit, :update, :destroy]
