ec99867da2613eed366310eaed060de13e7c6003
James Wu <wuj818@gmail.com>
2018-11-21 00:58:08 -0500

    fixing rubocop: Layout/TrailingWhitespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,22 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :asset_snapshots, path: 'asset-snapshots', only: [:show, :edit, :update, :destroy]
-  
+
   resources :contributions, only: [:index, :show, :edit, :update, :destroy]
-  
+
   resources :financial_assets, path: 'financial-assets' do
     resources :asset_snapshots, path: 'snapshots', as: 'snapshots', only: [:new, :create]
-  
+
     resources :contributions, only: [:new, :create]
   end
-  
+
   resources :milestones
-  
+
   get '/investments' => 'investments#history'
-  
+
   resources :sessions, only: [:new, :create, :destroy]
   get '/login' => 'sessions#new'
   delete '/logout' => 'sessions#destroy'
-  
+
   root to: 'pages#main'
 end
