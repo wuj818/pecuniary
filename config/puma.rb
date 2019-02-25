eb02f6a8bf05a05b16d911d854da92c98ce412e7
James Wu <wuj818@gmail.com>
2019-02-24 20:57:44 -0500

    Upgrade rails to 5.2.2

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
-if Rails.env.production?
+if ENV['RAILS_ENV'] == 'production'
   # Puma can serve each request in a thread from an internal thread pool.
   # The `threads` method setting takes two numbers: a minimum and maximum.
   # Any libraries that use thread pools should be configured to match
