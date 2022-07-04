b8a0b257f89d938c1e865b6973729087db2cfba2
James Wu <wuj818@gmail.com>
2022-07-04 19:51:26 -0400

    Upgrade to Rails 6.1

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby File.read('.ruby-version')
 
-gem 'rails', '~> 6.0.5'
+gem 'rails', '~> 6.1.0'
 
 gem 'pg'
 
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,20 +9,21 @@ Bundler.require(*Rails.groups)
 module Pecuniary
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    config.time_zone = 'Eastern Time (US & Canada)'
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    config.time_zone = "Eastern Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     config.action_view.field_error_proc = proc { |html_tag, _instance| html_tag }
 
-    if ENV['HTTP_AUTH'].present?
+    if ENV["HTTP_AUTH"].present?
       config.middleware.use(Rack::Auth::Basic) do |username, password|
-        ENV['HTTP_AUTH'].split(':') == [username, password]
+        ENV["HTTP_AUTH"].split(":") == [username, password]
       end
     end
   end
