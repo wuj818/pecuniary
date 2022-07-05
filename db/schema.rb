312c8775593422c52f39b2e88d536d0cb49b3945
James Wu <wuj818@gmail.com>
2022-07-04 20:36:08 -0400

    Add Active Storage migrations

--- db/schema.rb
+++ db/schema.rb
@@ -2,26 +2,53 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_220358) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_07_05_003549) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "contributions", force: :cascade do |t|
     t.bigint "financial_asset_id"
     t.bigint "amount", default: 0
     t.date "date"
     t.text "permalink"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.boolean "employer", default: false
     t.index ["employer"], name: "idx_33855_index_contributions_on_employer"
     t.index ["financial_asset_id"], name: "idx_33855_index_contributions_on_financial_asset_id"
@@ -30,8 +57,8 @@ ActiveRecord::Schema.define(version: 2018_12_14_220358) do
 
   create_table "financial_assets", force: :cascade do |t|
     t.text "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.text "permalink"
     t.bigint "current_value", default: 0
     t.bigint "total_contributions", default: 0
@@ -43,8 +70,8 @@ ActiveRecord::Schema.define(version: 2018_12_14_220358) do
     t.date "date"
     t.text "notes"
     t.text "permalink"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.text "cached_tag_list"
     t.index ["date"], name: "idx_33866_index_milestones_on_date", unique: true
     t.index ["permalink"], name: "idx_33866_index_milestones_on_permalink", unique: true
@@ -55,8 +82,8 @@ ActiveRecord::Schema.define(version: 2018_12_14_220358) do
     t.bigint "value", default: 0
     t.date "date"
     t.text "permalink"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["financial_asset_id"], name: "idx_33833_index_snapshots_on_financial_asset_id"
     t.index ["permalink"], name: "idx_33833_index_snapshots_on_permalink", unique: true
   end
@@ -68,7 +95,7 @@ ActiveRecord::Schema.define(version: 2018_12_14_220358) do
     t.bigint "tagger_id"
     t.text "tagger_type"
     t.text "context"
-    t.datetime "created_at"
+    t.datetime "created_at", precision: nil
     t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "idx_33885_taggings_idx", unique: true
     t.index ["taggable_id", "taggable_type", "context"], name: "idx_33885_index_taggings_on_taggable_id_and_taggable_type_and_c"
   end
@@ -79,4 +106,6 @@ ActiveRecord::Schema.define(version: 2018_12_14_220358) do
     t.index ["name"], name: "idx_33875_index_tags_on_name", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
