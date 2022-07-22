4fc0837897d9cb98c7c90757e9c21fd3433f6812
James Wu <wuj818@gmail.com>
2022-07-22 01:15:36 -0400

    Redo milestone system spec

--- spec/system/milestone_system_spec.rb
+++ spec/system/milestone_system_spec.rb
@@ -2,20 +2,20 @@
 
 require "rails_helper"
 
-RSpec.describe "Milestone System" do
+RSpec.describe "Milestone management" do
   context "when logged in" do
     before { system_spec_login }
 
     describe "milestones index" do
-      let!(:milestones) { create_list :milestone, 2 }
+      let!(:milestones) { create_list(:milestone, 2) }
 
       it "lists the milestones along with edit and delete links" do
         visit milestones_path
 
         milestones.each do |milestone|
-          expect(page).to have_link href: milestone_path(milestone)
-          expect(page).to have_link href: edit_milestone_path(milestone)
-          expect(page).to have_link id: dom_id(milestone, :delete)
+          expect(page).to have_link(href: milestone_path(milestone))
+          expect(page).to have_link(href: edit_milestone_path(milestone))
+          expect(page).to have_link(id: dom_id(milestone, :delete))
         end
       end
     end
@@ -33,9 +33,9 @@ RSpec.describe "Milestone System" do
         fill_in "milestone_notes", with: milestone_note
         click_button "Save"
 
-        expect(page.current_path).to eq milestones_path
+        expect(page.current_path).to eq(milestones_path)
         expect(page).to have_content(/successfully created/i)
-        expect(page).to have_content milestone_note
+        expect(page).to have_content(milestone_note)
       end
     end
 
@@ -43,7 +43,7 @@ RSpec.describe "Milestone System" do
       let(:milestone_note) { "test note" }
       let(:new_milestone_note) { "new test note" }
 
-      let!(:milestone) { create :milestone, notes: milestone_note }
+      let!(:milestone) { create(:milestone, notes: milestone_note) }
 
       it "allows the admin to update milestones and shows errors when appropriate" do
         visit milestones_path
@@ -57,46 +57,44 @@ RSpec.describe "Milestone System" do
         fill_in "milestone_notes", with: new_milestone_note
         click_button "Save"
 
-        expect(page.current_path).to eq milestone_path(milestone)
+        expect(page.current_path).to eq(milestone_path(milestone))
         expect(page).to have_content(/successfully updated/i)
-        expect(page).to have_content new_milestone_note
+        expect(page).to have_content(new_milestone_note)
       end
     end
 
     describe "milestone deletion" do
-      let!(:milestone) { create :milestone }
+      let!(:milestone) { create(:milestone) }
 
       it "deletes the milestone", js: true do
         visit milestones_path
 
-        accept_confirm do
-          click_link id: dom_id(milestone, :delete)
-        end
+        accept_confirm { click_link id: dom_id(milestone, :delete) }
 
-        expect(page.current_path).to eq milestones_path
+        expect(page.current_path).to(eq milestones_path)
         expect(page).to have_content(/successfully deleted/i)
-        expect(page).to have_no_link href: milestone_path(milestone)
+        expect(page).to have_no_link(href: milestone_path(milestone))
       end
     end
   end
 
   context "when logged out" do
     describe "milestones index" do
-      let!(:milestones) { create_list :milestone, 2 }
+      let!(:milestones) { create_list(:milestone, 2) }
 
       it "lists the milestones without edit or delete links" do
         visit milestones_path
 
         milestones.each do |milestone|
-          expect(page).to have_link href: milestone_path(milestone)
-          expect(page).to have_no_link href: edit_milestone_path(milestone)
-          expect(page).to have_no_link id: dom_id(milestone, :delete)
+          expect(page).to have_link(href: milestone_path(milestone))
+          expect(page).to have_no_link(href: edit_milestone_path(milestone))
+          expect(page).to have_no_link(id: dom_id(milestone, :delete))
         end
       end
     end
 
     describe "milestone creation and modification" do
-      let!(:milestone) { create :milestone }
+      let!(:milestone) { create(:milestone) }
 
       it "prevents access to the new and edit milestone pages" do
         visit new_milestone_path
