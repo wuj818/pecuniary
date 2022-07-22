2a83b6663fa43cddf7a8f306c8a882caa6be97a1
James Wu <wuj818@gmail.com>
2022-07-22 01:33:32 -0400

    Fix Rubocop: Naming/BlockForwarding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -51,7 +51,7 @@ module ApplicationHelper
     controller.to_s == controller_name ? :active : nil
   end
 
-  def page_header(&block)
-    tag.div(class: "pb-2 mb-4 border-bottom", &block)
+  def page_header(&)
+    tag.div(class: "pb-2 mb-4 border-bottom", &)
   end
 end
