7609ba781d43af308f4360c28a849e823d5ac009
James Wu <wuj818@gmail.com>
2022-07-22 01:10:41 -0400

    Redo session system spec

--- spec/system/session_system_spec.rb
+++ spec/system/session_system_spec.rb
@@ -2,25 +2,25 @@
 
 require "rails_helper"
 
-RSpec.describe "Session System" do
+RSpec.describe "Session management" do
   it "allows the admin to login and logout" do
     visit root_path
 
-    expect(page).to have_no_link "Logout"
+    expect(page).to have_no_link("Logout")
 
     click_link "Login"
     fill_in "password", with: "wrong"
     click_button "Login"
 
     expect(page).to have_content(/incorrect password/i)
-    expect(page).to have_no_link "Logout"
+    expect(page).to have_no_link("Logout")
 
-    fill_in "password", with: Rails.application.credentials.password[Rails.env.to_sym]
+    fill_in "password", with: admin_password
     click_button "Login"
 
     expect(page).to have_content(/logged in/i)
-    expect(page).to have_link "Logout"
-    expect(page).to have_no_link "Login"
+    expect(page).to have_link("Logout")
+    expect(page).to have_no_link("Login")
   end
 
   it "redirects away from the login form if already logged in" do
